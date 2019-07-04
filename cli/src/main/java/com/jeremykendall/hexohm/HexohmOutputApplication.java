272dc4d6ed339310d16134d4a352f3789a981663
jeremykendall <jeremy@jeremykendall.net>
2019-07-04 18:20:46 -0500

    BLUE: Add cli as subproject

---
+++ cli/src/main/java/com/jeremykendall/hexohm/HexohmOutputApplication.java
@@ -0,0 +1,12 @@
+package com.jeremykendall.hexohm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexohmOutputApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexohmOutputApplication.class, args);
+	}
+}
