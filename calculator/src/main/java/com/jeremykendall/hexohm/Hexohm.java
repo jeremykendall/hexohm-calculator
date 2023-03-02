add93f52572217aaebb64e9de981c873e1316e68
Jeremy Kendall <jeremy@jeremykendall.net>
2023-03-01 19:43:02 -0600

    One big update b/c I got lazy noodling on a personal project

--- calculator/src/main/java/com/jeremykendall/hexohm/Hexohm.java
+++ calculator/src/main/java/com/jeremykendall/hexohm/Hexohm.java
@@ -1,56 +1,71 @@
 package com.jeremykendall.hexohm;
 
-import com.jeremykendall.electricity.OhmsLaw;
+import com.jeremykendall.electricity.Amperage;
 import com.jeremykendall.electricity.Resistance;
 import com.jeremykendall.electricity.Voltage;
 import com.jeremykendall.electricity.Wattage;
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-@RequiredArgsConstructor
-public class Hexohm {
-
-    private static final Map<Integer, Voltage> OUTPUT_VOLTAGE_AT_SETTING = new LinkedHashMap<Integer, Voltage>() {{
-        put(0, Voltage.of(3.27));
-        put(10, Voltage.of(3.38));
-        put(20, Voltage.of(3.53));
-        put(30, Voltage.of(3.72));
-        put(40, Voltage.of(3.9));
-        put(50, Voltage.of(4.16));
-        put(60, Voltage.of(4.42));
-        put(70, Voltage.of(4.79));
-        put(80, Voltage.of(5.14));
-        put(90, Voltage.of(5.54));
-        put(100, Voltage.of(6.0));
-    }};
+@ToString
+final public class Hexohm {
 
     private static final Wattage MAX_WATTAGE = Wattage.of(180.0);
 
-    @NonNull
     @Getter
-    private final Resistance ohms;
+    @NonNull
+    private final Resistance coilResistance;
 
-    public Wattage getPowerOutput(int potentiometerSetting) {
+    @Getter
+    @NonNull
+    private final Potentiometer potentiometer;
 
-        validatePotentiometerSetting(potentiometerSetting);
+    private final double maxVoltage;
 
-        Wattage wattage = OhmsLaw.getWattage(getOutputVoltage(potentiometerSetting), ohms);
-
-        return wattage.isGreaterThan(MAX_WATTAGE) ? MAX_WATTAGE: wattage;
+    public Hexohm(Resistance coilResistance, Potentiometer potentiometer) {
+        this.coilResistance = coilResistance;
+        this.potentiometer = potentiometer;
+        this.maxVoltage = Math.sqrt(coilResistance.getOhms() * MAX_WATTAGE.getWatts());
     }
 
-    private void validatePotentiometerSetting(int potentiometerSetting) {
-        if (!OUTPUT_VOLTAGE_AT_SETTING.containsKey(potentiometerSetting)) {
-            throw new IllegalArgumentException("potentiometerSetting setting must be one of " +
-                    OUTPUT_VOLTAGE_AT_SETTING.keySet().toString());
+    public Wattage getPowerOutput() {
+        return Wattage.calculate(getAttenuatedVoltage(potentiometer), coilResistance, 2);
+    }
+
+    public Amperage getCurrent() {
+        return Amperage.calculate(getAttenuatedVoltage(potentiometer), coilResistance, 2);
+    }
+
+    private Voltage getAttenuatedVoltage(Potentiometer potentiometer) {
+        return Voltage.of(Math.min(potentiometer.voltage, this.maxVoltage));
+    }
+
+    public enum Potentiometer {
+        ZERO(0, 3.27),
+        TEN(10, 3.38),
+        TWENTY(20, 3.53),
+        THIRTY(30, 3.72),
+        FORTY(40, 3.9),
+        FIFTY(50, 4.16),
+        SIXTY(60, 4.42),
+        SEVENTY(70, 4.79),
+        EIGHTY(80, 5.14),
+        NINETY(90, 5.54),
+        ONE_HUNDRED(100, 6.0);
+
+        private final int setting;
+
+        private final double voltage;
+
+        Potentiometer(int setting, double voltage) {
+            this.setting = setting;
+            this.voltage = voltage;
         }
-    }
 
-    private Voltage getOutputVoltage(int potentiometerSetting) {
-        return OUTPUT_VOLTAGE_AT_SETTING.get(potentiometerSetting);
+        @Override
+        public String toString() {
+            return String.valueOf(setting);
+        }
     }
 }
--- calculator/src/test/groovy/com/jeremykendall/hexohm/HexohmSpec.groovy
+++ calculator/src/test/groovy/com/jeremykendall/hexohm/HexohmSpec.groovy
@@ -1,62 +1,87 @@
 package com.jeremykendall.hexohm
 
+import com.jeremykendall.electricity.Amperage
 import com.jeremykendall.electricity.Resistance
 import com.jeremykendall.electricity.Wattage
 import spock.lang.Specification
 import spock.lang.Unroll
+
+import static com.jeremykendall.hexohm.Hexohm.Potentiometer.EIGHTY
+import static com.jeremykendall.hexohm.Hexohm.Potentiometer.FIFTY
+import static com.jeremykendall.hexohm.Hexohm.Potentiometer.FORTY
+import static com.jeremykendall.hexohm.Hexohm.Potentiometer.NINETY
+import static com.jeremykendall.hexohm.Hexohm.Potentiometer.ONE_HUNDRED
+import static com.jeremykendall.hexohm.Hexohm.Potentiometer.SEVENTY
+import static com.jeremykendall.hexohm.Hexohm.Potentiometer.SIXTY
+import static com.jeremykendall.hexohm.Hexohm.Potentiometer.TEN
+import static com.jeremykendall.hexohm.Hexohm.Potentiometer.THIRTY
+import static com.jeremykendall.hexohm.Hexohm.Potentiometer.TWENTY
+import static com.jeremykendall.hexohm.Hexohm.Potentiometer.ZERO
+
 
 @Unroll
 class HexohmSpec extends Specification {
 
     def "it should calculate power output correctly"() {
         given:
-        Hexohm hexohm = new Hexohm(Resistance.of(ohms))
+        Hexohm hexohm = new Hexohm(Resistance.of(ohms), potentiometer)
 
         expect:
-        hexohm.getPowerOutput(potentiometerSetting) == Wattage.of(wattage)
+        hexohm.getPowerOutput() == Wattage.of(wattage)
 
         where:
-        potentiometerSetting | ohms | wattage
-        50                   | 0.2  | 87
-        100                  | 0.2  | 180
+        potentiometer | ohms | wattage
+        FIFTY         | 0.2  | 86.53
+        ONE_HUNDRED   | 0.2  | 180.0
+        THIRTY        | 0.25 | 55.35
+        ZERO          | 0.3  | 35.64
+        TEN           | 0.3  | 38.08
+        TWENTY        | 0.3  | 41.54
+        THIRTY        | 0.3  | 46.13
+        FORTY         | 0.3  | 50.7
+        FIFTY         | 0.3  | 57.69
+        SIXTY         | 0.3  | 65.12
+        SEVENTY       | 0.3  | 76.48
+        EIGHTY        | 0.3  | 88.07
+        NINETY        | 0.3  | 102.31
+        ONE_HUNDRED   | 0.3  | 120.0
+        ONE_HUNDRED   | 0.5  | 72.0
+    }
 
-        30                   | 0.25 | 55
+    def "it should calculate current draw correctly"() {
+        given:
+        Hexohm hexohm = new Hexohm(Resistance.of(ohms), potentiometer)
 
-        0                    | 0.3  | 36
-        10                   | 0.3  | 38
-        20                   | 0.3  | 42
-        30                   | 0.3  | 46
-        40                   | 0.3  | 51
-        50                   | 0.3  | 58
-        60                   | 0.3  | 65
-        70                   | 0.3  | 76
-        80                   | 0.3  | 88
-        90                   | 0.3  | 102
-        100                  | 0.3  | 120
+        expect:
+        hexohm.getCurrent() == Amperage.of(amps)
 
-        100                  | 0.5  | 72
+        where:
+        potentiometer | ohms | amps
+        FIFTY         | 0.2  | 20.8
+        ONE_HUNDRED   | 0.2  | 30.0
+        THIRTY        | 0.25 | 14.88
+        ZERO          | 0.3  | 10.9
+        TEN           | 0.3  | 11.27
+        TWENTY        | 0.3  | 11.77
+        THIRTY        | 0.3  | 12.4
+        FORTY         | 0.3  | 13.0
+        FIFTY         | 0.3  | 13.87
+        SIXTY         | 0.3  | 14.73
+        SEVENTY       | 0.3  | 15.97
+        EIGHTY        | 0.3  | 17.13
+        NINETY        | 0.3  | 18.47
+        ONE_HUNDRED   | 0.3  | 20.0
+        ONE_HUNDRED   | 0.5  | 12.0
     }
 
     def "wattage should never exceed 180 watts"() {
         given:
-        def hexohm = new Hexohm(Resistance.of(0.1))
+        def hexohm = new Hexohm(Resistance.of(0.1), ONE_HUNDRED)
 
         when:
-        Wattage wattage = hexohm.getPowerOutput(60)
+        Wattage wattage = hexohm.getPowerOutput()
 
         then:
         wattage == Wattage.of(180.0)
-    }
-
-    def "should throw exception if potentiometer setting isn't 0 - 100 (counted by 10s)"() {
-        given:
-        Hexohm hexohm = new Hexohm(Resistance.of(0.22))
-
-        when:
-        hexohm.getPowerOutput(25)
-
-        then:
-        IllegalArgumentException e = thrown()
-        e.getMessage() == "potentiometerSetting setting must be one of [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]"
     }
 }
